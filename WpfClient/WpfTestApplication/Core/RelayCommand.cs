63a68514af72172c747575f8fd74614a8a369108
TVallante01 <tvallante01@gmail.com>
2023-03-22 13:27:25 -0400

    Initial Wpf Commit
    Testing Wpf visuals with html and css

---
+++ WpfClient/WpfTestApplication/Core/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfTestApplication.Core
+{
+    class RelayCommand : ICommand
+    {
+        private Action<Object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested += value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
