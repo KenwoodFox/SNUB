d0ae16f1600b96c9e7a25a39db54721b2e1436ed
TVallante01 <tvallante01@gmail.com>
2023-03-29 11:30:46 -0400

    wpf exit commit

--- WpfClient/WpfTestApplication/MainWindow.xaml.cs
+++ WpfClient/WpfTestApplication/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfTestApplication
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
