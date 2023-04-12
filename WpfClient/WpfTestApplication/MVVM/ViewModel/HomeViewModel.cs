04e6e9aa9aa54a697cd4173404cdef9b909e1064
TVallante01 <tvallante01@gmail.com>
2023-04-12 12:02:00 -0400

    Wpf Changes

---
+++ WpfClient/WpfTestApplication/MVVM/ViewModel/HomeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using WpfTestApplication.Core;
+
+namespace WpfTestApplication.MVVM.ViewModel {
+    class HomeViewModel : ObservableObject 
+    {
+
+
+        public HomeViewModel HomeVM { get; set; }   
+
+        private object currentView;
+
+        public object CurrentView
+        {
+            get { return currentView; }
+            set 
+            { 
+                currentView = value; 
+                OnPropertyChanged();
+            }
+        }
+        public HomeViewModel() 
+        {
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
+        }
+    }
+}
